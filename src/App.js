4504af60626644d049c237cd8929f713b1d04c00
Avinash Kumar <ak2449746@gmail.com>
2022-05-03 19:29:04 +0530

    update...

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ import SearchIcon from './search.svg';
 
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=586b4a8";
 
-// const movie1 = {
-//   "Title": "Spiderman",
-//   "Year": "1990",
-//   "imdbID": "tt0100669",
-//   "Type": "movie",
-//   "Poster": "N/A"
-// }
-
 const App = () => {
 
   const [movies, setMovies] = useState([]);
